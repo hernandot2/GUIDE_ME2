9de33571f270a0c00641e968f1aac4633e424f19
3por5 <73372271+3por5@users.noreply.github.com>
2023-07-25 22:07:36 -0300

    criado model e controller de programs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+user = User.new(email: "pedro@teste.com.br", password: "123123")
+user.save!
+
+program = Program.new(user: user, category: "rails", name: "aprenda rails", description: "esse programa é massa")
+program.save!
+program = Program.new(user: user, category: "ruby", name: "aprenda ruby", description: "esse programa também é massa")
+program.save!
