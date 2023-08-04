714617408b260df5a592f0208ad93cdefbe91b91
Julia Artigas <juju_artigas@hotmail.com>
2023-08-03 22:09:37 -0300

    add controller mentorship pundit

--- app/models/program.rb
+++ app/models/program.rb
@@ -1,6 +1,6 @@
 class Program < ApplicationRecord
   belongs_to :user
-  has_many :mentorships
+  has_many :mentorships, dependent: :destroy
 
   validates :name, presence: true
   validates :category, presence: true
