1343d121ad4a7db6788321b0d40eb4c7bb383e35
3por5 <73372271+3por5@users.noreply.github.com>
2023-08-03 22:14:33 -0300

    work in progress

--- app/controllers/mentorships_controller.rb
+++ app/controllers/mentorships_controller.rb
@@ -1,2 +1,30 @@
 class MentorshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def show
+  end
+
+  def create
+    program = Program.find(params[:program_id])
+    mentorship = Mentorship.new(user: current_user, program: program)
+
+    if mentorship.save
+      flash[:success] = "você se inscreveu neste programa!"
+    else
+      flash[:error] = "algo deu errado"
+    end
+
+    redirect_to program_path(program)
+  end
+
+  def destroy
+    mentorship = Mentorship.find(params[:id])
+    if mentorship.user == current_user
+      mentorship.destroy
+      flash[:success] = "você se desinscreveu do programa"
+    else
+      flash[:error] = "você não tem autorização para se desinscrever"
+    end
+    redirect_to profiles_path
+  end
 end
